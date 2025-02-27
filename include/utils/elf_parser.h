38792556076a5c9864455d81b1a8ae94199a0212
puranikvinit <kvp933.vinit@gmail.com>
2025-02-27 19:13:28 +0530

    [elf_parser] minor args and code formatting fixes

--- include/utils/elf_parser.h
+++ include/utils/elf_parser.h
@@ -1,11 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#ifndef OUTPUT_FILE_PATH
+#define OUTPUT_FILE_PATH "instructions.txt"
+#endif
+
+#include <elf.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
-#include <elf.h>
 
 /**
  * @brief Extracts instructions from an ELF file and writes them to a text file
@@ -17,6 +21,6 @@
  * @param elf_file_path Path to the input ELF file
  * @param output_file_path Path to the output text file
  */
-void extract_instructions(const char* elf_file_path, const char* output_file_path);
+void extract_instructions(const char *elf_file_path);
 
-#endif //!PARSER_H
+#endif //! PARSER_H
