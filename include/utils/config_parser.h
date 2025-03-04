29b54047422dc1c9b06157ca883f5c247076c38e
puranikvinit <kvp933.vinit@gmail.com>
2025-03-04 22:55:34 +0530

    [utils] tweak config_parser for easier usage

--- include/utils/config_parser.h
+++ include/utils/config_parser.h
@@ -1,6 +1,7 @@
 #ifndef CONFIG_PARSER_H
 #define CONFIG_PARSER_H
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <yaml.h>
@@ -9,34 +10,17 @@
 #define CONFIG_FILE_PATH "config.yaml"
 #endif
 
-#define CONFIG_MAP_SIZE 100
-#define CONFIG_MAX_KEY_SIZE 256
-#define CONFIG_MAX_VALUE_SIZE 256
-
-typedef struct {
-  char key[CONFIG_MAX_KEY_SIZE];
-  char value[CONFIG_MAX_VALUE_SIZE];
-} hash_entry;
-
-typedef struct {
-  hash_entry table[CONFIG_MAP_SIZE];
-  int slot_used[CONFIG_MAP_SIZE];
-} hash_map;
+#ifndef CONFIG_DEFINES_FILE
+#define CONFIG_DEFINES_FILE "include/config_defines.h"
+#endif
 
 typedef enum { INFO, DEBUG, WARN, ERROR } verbosity_levels;
 
-typedef struct {
-  hash_map cpu_conf;
-  hash_map mem_conf;
-  hash_map stat_conf;
-  hash_map log_conf;
-} configs;
+// 0: success
+// 1: file read failed
+// 2: parser init failed
+int parse_configs();
 
-unsigned int _hash_function(const char *str);
-int _hash_map_set(hash_map *map, const char *key, const char *value);
-const char *_hash_map_get(hash_map *map, const char *key);
-void _hash_map_init(hash_map *map);
-
-configs *parse_configs();
+void _str_to_upper(char *str);
 
 #endif // !CONFIG_PARSER_H
--- src/utils/config_parser.c
+++ src/utils/config_parser.c
@@ -1,81 +1,30 @@
 #include "utils/config_parser.h"
 
-configs parsed_configs;
-
-hash_map cpu_config_hash_map;
-hash_map mem_config_hash_map;
-hash_map stats_config_hash_map;
-hash_map logs_config_hash_map;
-
-unsigned int _hash_function(const char *str) {
-  unsigned int hash = 5381;
-  int c;
-  while ((c = *str++))
-    hash = ((hash << 5) + hash) + c; // hash * 33 + c
-  return hash % CONFIG_MAP_SIZE;
+void _str_to_upper(char *str) {
+  for (int i = 0; str[i] != '\0'; i++)
+    str[i] = toupper(str[i]);
 }
 
-int _hash_map_set(hash_map *map, const char *key, const char *value) {
-  unsigned int index = _hash_function(key);
-
-  while (map->slot_used[index]) {
-    if (strncmp(map->table[index].key, key, strlen(key)) == 0) {
-      strcpy(map->table[index].value, value);
-      return 1;
-    }
-    index = (index + 1) % CONFIG_MAP_SIZE;
-  }
-
-  strcpy(map->table[index].key, key);
-  strcpy(map->table[index].value, value);
-  map->slot_used[index] = 1;
-
-  return 0;
-}
-
-const char *_hash_map_get(hash_map *map, const char *key) {
-  unsigned int index = _hash_function(key);
-
-  while (map->slot_used[index]) {
-    if (strcmp(map->table[index].key, key) == 0) {
-      return map->table[index].value;
-    }
-    index = (index + 1) % CONFIG_MAP_SIZE;
-  }
-
-  return NULL;
-}
-
-void _hash_map_init(hash_map *map) {
-  for (int i = 0; i < CONFIG_MAP_SIZE; i++)
-    map->slot_used[i] = 0;
-
-  int init_status = 0;
-  _hash_map_set(&cpu_config_hash_map, "pipeline_stages", "5");
-
-  _hash_map_set(&mem_config_hash_map, "l1_icache_size", "128");
-  _hash_map_set(&mem_config_hash_map, "l1_dcache_size", "128");
-  _hash_map_set(&mem_config_hash_map, "l2_cache_size", "1024");
-  _hash_map_set(&mem_config_hash_map, "dram_size", "4096");
-
-  _hash_map_set(&stats_config_hash_map, "cache_misses", "1");
-  _hash_map_set(&stats_config_hash_map, "branch_mispredictions", "1");
-  _hash_map_set(&stats_config_hash_map, "memory_heatmap", "0");
-  _hash_map_set(&stats_config_hash_map, "register_heatmap", "0");
-
-  _hash_map_set(&logs_config_hash_map, "verbosity_level", "3");
-}
-
-configs *parse_configs() {
+int parse_configs() {
   yaml_parser_t parser;
   yaml_token_t token;
 
   FILE *config_file = fopen(CONFIG_FILE_PATH, "r");
-  if (config_file == NULL)
+  if (config_file == NULL) {
     printf("config file read failed");
+    return 1;
+  }
 
-  if (!yaml_parser_initialize(&parser))
+  FILE *config_defines_file = fopen(CONFIG_DEFINES_FILE, "w");
+  if (config_defines_file == NULL) {
+    printf("config defines file open failed");
+    return 1;
+  }
+
+  if (!yaml_parser_initialize(&parser)) {
     printf("config parser init failed");
+    return 2;
+  }
 
   yaml_parser_set_input_file(&parser, config_file);
 
@@ -123,21 +72,14 @@ configs *parse_configs() {
         block_name = strdup((char *)token.data.scalar.value);
 
       else if (block_mapping_level == 2) {
-        if (is_scalar_key)
+        if (is_scalar_key) {
           key_name = strdup((char *)token.data.scalar.value);
+          _str_to_upper(key_name);
+        }
 
         else {
           char *value = (char *)token.data.scalar.value;
-          if (strncmp(block_name, "cpu", strlen("cpu")) == 0) {
-            _hash_map_set(&cpu_config_hash_map, key_name, value);
-          } else if (strncmp(block_name, "memory", strlen("memory")) == 0) {
-            _hash_map_set(&mem_config_hash_map, key_name, value);
-          } else if (strncmp(block_name, "enable_stats",
-                             strlen("enable_stats")) == 0) {
-            _hash_map_set(&stats_config_hash_map, key_name, value);
-          } else if (strncmp(block_name, "logs", strlen("logs")) == 0) {
-            _hash_map_set(&logs_config_hash_map, key_name, value);
-          }
+          fprintf(config_defines_file, "#define %s %s\n", key_name, value);
         }
       }
       break;
@@ -152,13 +94,9 @@ configs *parse_configs() {
 
   yaml_parser_delete(&parser);
   fclose(config_file);
+  fclose(config_defines_file);
   free(block_name);
   free(key_name);
 
-  parsed_configs.cpu_conf = cpu_config_hash_map;
-  parsed_configs.mem_conf = mem_config_hash_map;
-  parsed_configs.stat_conf = stats_config_hash_map;
-  parsed_configs.log_conf = logs_config_hash_map;
-
-  return &parsed_configs;
+  return 0;
 }
